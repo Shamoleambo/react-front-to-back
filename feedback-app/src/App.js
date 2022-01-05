e83767c586c537e85324d460602f89d0b59976e2
Tiago Gomes <tidgomes@gmail.com>
2022-01-05 14:49:19 -0300

    default properties and types of the properties

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,11 +3,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header
-        text={
-          "i dont need to use propTypes, but if i use typescript... anyway: type checking is coool"
-        }
-      />
+      <Header />
       <div className="container">
         <h1>My App</h1>
       </div>
