6303a6afd7e8eb427df8be4a0cb0adf36b0d36d1
Tiago Gomes <tidgomes@gmail.com>
2022-01-05 15:48:13 -0300

    managing global state

---
+++ feedback-app/src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "omg omg omg omg omg omg omg omg omg omg omg",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "complete shit bra... but u cool",
+  },
+  {
+    id: 3,
+    rating: 7,
+    text: "best car ever",
+  },
+];
+
+export default FeedbackData;
